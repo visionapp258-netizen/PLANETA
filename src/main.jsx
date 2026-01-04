8cdfb7a24a4e7954ed04ac15a8e0ff35c4680e88
Titos +258 <visionapp258@gmail.com>
2026-01-04 10:37:28 +0000

    estrutura basica do codigo

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './Planeta.jsx' // Certifica-te que o nome do ficheiro é exatamente este
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
